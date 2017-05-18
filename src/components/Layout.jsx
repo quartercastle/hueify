74b9c262526e68af34f8d3981740f776063bf769
Frederik Kvartborg <fka@skemax.dk>
2017-05-18 22:52:30 +0200

    Simplified layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,21 +1,11 @@
-import { Component } from 'preact'
 import Header from 'components/Header'
 import Content from 'components/Content'
 
-export default class Layout extends Component {
-  /**
-   * Render Layout
-   * @param {Object} props
-   * @return {JSX}
-   */
-  render ({ children }) {
-    return (
-      <div>
-        <Header />
-        <Content>
-          {children}
-        </Content>
-      </div>
-    )
-  }
-}
+export default ({ children }) => (
+  <div>
+    <Header />
+    <Content>
+      {children}
+    </Content>
+  </div>
+)
